de014c97620f800f84e5657de025efae4e2c3979
adejazzmind <adejazzmind@gmail.com>
2025-09-22 09:37:52 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Alarm
+{
+    // Declare the event
+    public event Action AlarmRang;
+
+    // Method that triggers the event
+    public void Ring()
+    {
+        Console.WriteLine("Alarm is ringing!");
+        AlarmRang?.Invoke();  // Raise the event
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create an instance of Alarm
+        Alarm alarm = new Alarm();
+
+        // Subscribe to the event with an event handler (lambda)
+        alarm.AlarmRang += () => Console.WriteLine("Wake up!");
+
+        // Call the Ring method
+        alarm.Ring();
+
+        Console.ReadLine();
+    }
+}
